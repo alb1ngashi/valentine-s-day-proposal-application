d52bad4d1a4fad36d431cc8ff517334c33b70014
Vibesdk <vibesdk-bot@cloudflare.com>
2026-02-06 07:39:13 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun install canvas-confetti @types/canvas-confetti"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
